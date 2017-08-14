185a703924f51699f46a4c5ee242a36d573186b8
Ben Kremer <bk@qundg.de>
2017-08-14 12:23:07 +0200

    added baiscs

--- src/Core/Sloth.php
+++ src/Core/Sloth.php
@@ -1,6 +1,42 @@
 <?php
+
 namespace Sloth\Core;
 
 class Sloth extends \Singleton {
 
+	public $container;
+
+	public function __construct() {
+		/*
+		 * Instantiate the service container for the project.
+		 */
+		$this->container = new \Sloth\Core\Application();
+
+		/*
+         * Setup the facade.
+         */
+		\Sloth\Facades\Facade::setFacadeApplication( $this->container );
+
+
+		$this->registerProviders();
+	}
+
+	/**
+	 * Register core framework service providers.
+	 */
+	protected function registerProviders() {
+		/*
+		 * Service providers.
+		 */
+		$providers = [
+			#\Sloth\Route\RouteServiceProvider::class,
+			#\Sloth\Finder\FinderServiceProvider::class,
+			#\Sloth\View\ViewServiceProvider::class,
+			#\Sloth\Module\ModuleServiceProvider::class,
+		];
+
+		foreach ( $providers as $provider ) {
+			$this->container->register( $provider );
+		}
+	}
 }
---
+++ src/Facades/Facade.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Sloth\Facades;
+
+abstract class Facade {
+	/**
+	 * The Application instance.
+	 *
+	 * @var \Sloth\Core\Application
+	 */
+	protected static $app;
+
+	/**
+	 * Set the service container for the facades.
+	 *
+	 * @param \Sloth\Core\Application $app
+	 */
+	public static function setFacadeApplication( $app ) {
+		static::$app = $app;
+	}
+
+	/**
+	 * Retrieve an instance from the container based on the
+	 * alias defined in the facade.
+	 *
+	 * @return mixed
+	 */
+	public static function getInstance() {
+		$name = static::getFacadeAccessor();
+
+		return static::$app[ $name ];
+	}
+
+	/**
+	 * Each facade must define their igniter service
+	 * class key name.
+	 *
+	 * @throws \RuntimeException
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() {
+		throw new \RuntimeException( 'Facade does not implement the "getFacadeAccessor" method.' );
+	}
+
+	/**
+	 * Magic method. Use to dynamically call the registered
+	 * instance method.
+	 *
+	 * @param string $method The class method used.
+	 * @param array $args The method arguments.
+	 *
+	 * @return mixed
+	 */
+	public static function __callStatic( $method, $args ) {
+		$instance = static::getInstance();
+
+		/*
+		 * Call the instance and its method.
+		 */
+
+		return call_user_func_array( [ $instance, $method ], $args );
+	}
+}
