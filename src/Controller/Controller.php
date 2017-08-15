8162017e4275098177ec0fd8013bbd4b9ada78f3
Ben Kremer <bk@qundg.de>
2017-08-15 14:39:01 +0200

    initial stub for controllers

---
+++ src/Controller/Controller.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kremer
+ * Date: 03.04.17
+ * Time: 01:39
+ */
+
+namespace Sloth\Controller;
+
+
+abstract class Controller {
+	public function beforeRender() {
+
+	}
+
+	public function afterRender() {
+
+	}
+}
