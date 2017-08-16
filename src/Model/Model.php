17b40ac2ffd0471c2a8fa8f376d4c5248b503de9
Ben Kremer <bk@qundg.de>
2017-08-16 10:43:28 +0200

    added PostType

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -1,8 +1,17 @@
 <?php
+
 namespace Sloth\Model;
 
-use Corcel\Post as Corcel;
+use Corcel\Model\Post as Corcel;
+use PostTypes\PostType;
 
 class Model extends Corcel {
+	protected $names = [];
+	protected $options = [];
+	public function register() {
+		$names = array_merge($this->names, ['name' => $this->getPostType()]);
+		$options = array_merge($this->options, ['menu_icon' => 'dashicons-' . preg_replace('/^dashicons-/', '', $this->icon)]);
 
+		new PostType($names, $options);
+	}
 }
