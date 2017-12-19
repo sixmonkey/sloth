1793d438ec3504103bd85b7f061b6ea0f57c9bb3
Ben Kremer <bk@qundg.de>
2017-12-19 14:25:47 +0100

    simplified postType

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -8,10 +8,20 @@ use PostTypes\PostType;
 class Model extends Corcel {
 	protected $names = [];
 	protected $options = [];
-	public function register() {
-		$names = array_merge($this->names, ['name' => $this->getPostType()]);
-		$options = array_merge($this->options, ['menu_icon' => 'dashicons-' . preg_replace('/^dashicons-/', '', $this->icon)]);
 
-		new PostType($names, $options);
+	public function __construct( array $attributes = [] ) {
+		if ( $this->postType == null ) {
+			$reflection = new \ReflectionClass($this);
+			$this->postType = $reflection->getShortName();
+		}
+		parent::__construct( $attributes );
+	}
+
+	public function register() {
+		$names = array_merge( $this->names, [ 'name' => $this->getPostType() ] );
+		$options = array_merge( $this->options,
+			[ 'menu_icon' => 'dashicons-' . preg_replace( '/^dashicons-/', '', $this->icon ) ] );
+
+		new PostType( $names, $options );
 	}
 }
