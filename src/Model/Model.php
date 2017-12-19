c66864418da675b3790f05f746dd3f0ee4c6106d
Ben Kremer <bk@qundg.de>
2017-12-19 14:49:26 +0100

    Added labels

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -8,11 +8,12 @@ use PostTypes\PostType;
 class Model extends Corcel {
 	protected $names = [];
 	protected $options = [];
+	protected $labels = [];
 
 	public function __construct( array $attributes = [] ) {
 		if ( $this->postType == null ) {
-			$reflection = new \ReflectionClass($this);
-			$this->postType = strtolower($reflection->getShortName());
+			$reflection     = new \ReflectionClass( $this );
+			$this->postType = strtolower( $reflection->getShortName() );
 		}
 		if ( $this->icon == null ) {
 			$this->icon = 'admin-post';
@@ -21,10 +22,11 @@ class Model extends Corcel {
 	}
 
 	public function register() {
-		$names = array_merge( $this->names, [ 'name' => $this->getPostType() ] );
+		$names   = array_merge( $this->names, [ 'name' => $this->getPostType() ] );
 		$options = array_merge( $this->options,
 			[ 'menu_icon' => 'dashicons-' . preg_replace( '/^dashicons-/', '', $this->icon ) ] );
+		$labels  = $this->labels;
 
-		new PostType( $names, $options );
+		new PostType( $names, $options, $labels );
 	}
 }
