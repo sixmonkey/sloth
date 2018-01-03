9bb64b6adb52c7d4b6e4f09f0bf253c74755babe
Ben Kremer <bk@qundg.de>
2018-01-03 22:30:07 +0100

    finally correct line numbers!

--- src/functions.php
+++ src/functions.php
@@ -1,6 +1,13 @@
 <?php
-if(!function_exists('debug')) {
-	function debug($var) {
-		bdump($var);
+if ( ! function_exists( 'debug' ) ) {
+	/**
+	 * Tracy\Debugger::barDump() shortcut.
+	 *
+	 * @tracySkipLocation
+	 */
+	function debug( $var ) {
+		call_user_func_array( 'Tracy\Debugger::barDump', func_get_args() );
+
+		return $var;
 	}
 }
