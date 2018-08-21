11fa56e5ec4d588c7c0098785105c18ccc3595a7
Ben Kremer <bk@qundg.de>
2018-08-21 14:18:01 +0200

    made dotenv optional

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -72,10 +72,11 @@ require_once( DIR_VENDOR . DS . 'autoload.php' );
 /**
  * Use Dotenv to set required environment variables and load .env file in root
  */
-$dotenv = new Dotenv\Dotenv( DIR_ROOT );
-$dotenv->load();
-$dotenv->required( [ 'DB_NAME', 'DB_USER', 'DB_PASSWORD', 'WP_HOME', 'WP_SITEURL' ] );
-
+if ( file_exists( DIR_ROOT . '.env' ) ) {
+	$dotenv = new Dotenv\Dotenv( DIR_ROOT );
+	$dotenv->load();
+	$dotenv->required( [ 'DB_NAME', 'DB_USER', 'DB_PASSWORD', 'WP_HOME', 'WP_SITEURL' ] );
+}
 /**
  * env config
  */
