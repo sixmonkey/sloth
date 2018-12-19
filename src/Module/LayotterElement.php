acd3994845e33eeb56cd6aebb73d81d2acecdfd9
Ben Kremer <ben.kremer@me.com>
2018-12-19 13:32:39 +0100

    enable rendering in admin

--- src/Module/LayotterElement.php
+++ src/Module/LayotterElement.php
@@ -45,9 +45,8 @@ class LayotterElement extends \Layotter_Element {
 		$module_name = $class_name::$module;
 		$module      = new $module_name;
 		$module->set( $fields );
-		if ( ! is_admin() ) {
-			$module->render();
-		}
+
+		$module->render();
 	}
 
 	public function backend_view( $fields ) {
@@ -86,7 +85,7 @@ class LayotterElement extends \Layotter_Element {
 	// @TODO: Should be in Module?
 	final protected function prepare_fields( $values ) {
 		$fields = $this->get_fields();
-		if ( Configure::read( 'layotter_prepare_fields' ) && $fields) {
+		if ( Configure::read( 'layotter_prepare_fields' ) && $fields ) {
 			foreach ( $fields as $field ) {
 				if ( $field['type'] == 'image' ) {
 					$v                        = new Image( $values[ $field['name'] ] );
