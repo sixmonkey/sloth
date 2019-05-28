9ea4f853e34c6fe91e04526f446fe997abae6475
Ben Kremer <ben.kremer@me.com>
2019-05-28 12:54:07 +0200

    pagination for rest

--- src/Pagination/Paginator.php
+++ src/Pagination/Paginator.php
@@ -5,24 +5,31 @@ namespace Sloth\Pagination;
 use Illuminate\Pagination\LengthAwarePaginator as BasePaginator;
 
 class Paginator extends BasePaginator {
-	/**
-	 * @TODO: This one seems very insecure?
-	 *
-	 * @param int $page
-	 *
-	 * @return string
-	 */
-	public function url( $page ) {
-		if ( \is_archive() ) {
-			return get_pagenum_link( $page );
-		}
+    /**
+     * @TODO: This one seems very insecure?
+     *
+     * @param int $page
+     *
+     * @return string
+     */
+    public function url( $page ) {
+        if ( \is_archive() ) {
+            return get_pagenum_link( $page );
+        }
 
-		$parts = [ rtrim( get_permalink(), '/' ) ];
-		if ( $page > 1 ) {
-			$parts[] = $page;
-		}
+        if ( defined( 'REST_REQUEST' ) && REST_REQUEST ) {
+            $current         = $_GET;
+            $current['page'] = $page;
 
-		return rtrim( implode( '/', $parts ), '/' ) . '/';
-	}
+            return parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH ) . '?' . http_build_query( $current );
+        }
+
+        $parts = [ rtrim( get_permalink(), ' / ' ) ];
+        if ( $page > 1 ) {
+            $parts[] = $page;
+        }
+
+        return rtrim( implode( ' / ', $parts ), ' / ' ) . ' / ';
+    }
 
 }
