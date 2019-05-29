b788d217b91cfeb0978a35bb6ef69165ca660edd
Ben Kremer <ben.kremer@me.com>
2019-05-30 01:53:58 +0200

    fixed bug in normalize

--- src/Utility/Utility.php
+++ src/Utility/Utility.php
@@ -11,15 +11,15 @@ use \Cake\Utility\Inflector;
 
 class Utility extends Inflector {
 
-	/**
-	 * @param $name
-	 *
-	 * @return bool|mixed|string
-	 */
-	public static function normalize( $name ) {
-		if ( strstr( '\\', $name ) ) {
-			$name = substr( strrchr( $name, "\\" ), 1 );
-		}
+    /**
+     * @param $name
+     *
+     * @return string
+     */
+    public static function normalize( $name ) {
+        if ( strstr( $name, '\\' ) ) {
+            $name = substr( strrchr( $name, "\\" ), 1 );
+        }
 
         $name = preg_replace( '/Module$/',
             '',
@@ -29,87 +29,87 @@ class Utility extends Inflector {
             '-',
             $name );
 
-		return $name;
-	}
+        return $name;
+    }
 
-	/**
-	 * return a module name
-	 *
-	 * @param      $name
-	 * @param bool $namespaced
-	 *
-	 * @return string
-	 */
-	public static function modulize( $name, $namespaced = false ) {
-		$name = self::normalize( $name );
+    /**
+     * return a module name
+     *
+     * @param      $name
+     * @param bool $namespaced
+     *
+     * @return string
+     */
+    public static function modulize( $name, $namespaced = false ) {
+        $name = self::normalize( $name );
 
-		$name = self::camelize( str_replace( '-',
-				'_',
-				$name ) ) . 'Module';
+        $name = self::camelize( str_replace( '-',
+                '_',
+                $name ) ) . 'Module';
 
-		if ( $namespaced ) {
-			$name = 'Theme\Module\\' . $name;
-		}
+        if ( $namespaced ) {
+            $name = 'Theme\Module\\' . $name;
+        }
 
-		return $name;
-	}
+        return $name;
+    }
 
-	/**
-	 * @param $name
-	 *
-	 * @return string
-	 */
-	public static function viewize( $name ) {
-		$name = self::normalize( $name );
+    /**
+     * @param $name
+     *
+     * @return string
+     */
+    public static function viewize( $name ) {
+        $name = self::normalize( $name );
 
-		$name = self::dasherize( $name );
+        $name = self::dasherize( $name );
 
-		return $name;
-	}
+        return $name;
+    }
 
-	/**
-	 * @param $name
-	 * @param $prefixed
-	 *
-	 * @return string
-	 */
-	public static function acfize( $name, $prefixed = true ) {
-		$name = self::normalize( $name );
+    /**
+     * @param $name
+     * @param $prefixed
+     *
+     * @return string
+     */
+    public static function acfize( $name, $prefixed = true ) {
+        $name = self::normalize( $name );
 
-		$name = self::underscore( $name );
-		if ( $prefixed ) {
-			$name = 'group_module_' . $name;
-		}
+        $name = self::underscore( $name );
+        if ( $prefixed ) {
+            $name = 'group_module_' . $name;
+        }
 
-		return $name;
-	}
+        return $name;
+    }
 
-	/**
-	 * make a float to a fraction
-	 *
-	 * @param       $n
-	 * @param float $tolerance
-	 *
-	 * @return string
-	 */
-	public static function float2fraction( $n, $tolerance = 1.e-6 ) {
-		$h1 = 1;
-		$h2 = 0;
-		$k1 = 0;
-		$k2 = 1;
-		$b  = 1 / $n;
-		do {
-			$b   = 1 / $b;
-			$a   = floor( $b );
-			$aux = $h1;
-			$h1  = $a * $h1 + $h2;
-			$h2  = $aux;
-			$aux = $k1;
-			$k1  = $a * $k1 + $k2;
-			$k2  = $aux;
-			$b   = $b - $a;
-		} while ( abs( $n - $h1 / $k1 ) > $n * $tolerance );
+    /**
+     * make a float to a fraction
+     *
+     * @param       $n
+     * @param float $tolerance
+     *
+     * @return string
+     */
+    public static function float2fraction( $n, $tolerance = 1.e-6 ) {
+        $h1 = 1;
+        $h2 = 0;
+        $k1 = 0;
+        $k2 = 1;
+        $b  = 1 / $n;
+        do {
+            $b   = 1 / $b;
+            $a   = floor( $b );
+            $aux = $h1;
+            $h1  = $a * $h1 + $h2;
+            $h2  = $aux;
+            $aux = $k1;
+            $k1  = $a * $k1 + $k2;
+            $k2  = $aux;
+            $b   = $b - $a;
+        } while ( abs( $n - $h1 / $k1 ) > $n * $tolerance );
 
-		return "$h1/$k1";
-	}
+        return "$h1/$k1";
+    }
 }
