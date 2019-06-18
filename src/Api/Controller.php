66e06535a92d3f6091203e509e8b673b0da19c2b
Ben Kremer <ben.kremer@me.com>
2019-06-18 14:55:57 +0200

    simplified getting urls

--- src/Api/Controller.php
+++ src/Api/Controller.php
@@ -2,6 +2,8 @@
 
 namespace Sloth\Api;
 
+use Sloth\Utility\Utility;
+
 class Controller {
     protected $request;
     public $response;
@@ -16,7 +18,22 @@ class Controller {
         $this->response->headers = [];
     }
 
-    function setRequest( \WP_REST_Request $request ) {
+    final public function setRequest( \WP_REST_Request $request ) {
         $this->request = $request;
     }
+
+    final protected function getUrl( string $path, array $params = [] ) {
+
+        parse_str( parse_url( $path, PHP_URL_QUERY ), $get_array );
+
+        $params = array_merge( $get_array, $params );
+
+        $path = '/sloth/v1/' . Utility::viewize( ( new \ReflectionClass( $this ) )->getShortName() ) . '/' . $path;
+        if ( count( $params ) ) {
+            $path .= '?' . http_build_query( $params );
+        }
+
+        return get_rest_url( null, $path );
+    }
+
 }
