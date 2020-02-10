3c34824e2d3a8be762947eb3e5272249f35b47ed
Ben Kremer <ben.kremer@me.com>
2020-02-10 14:52:36 +0100

    restored beards

--- src/Plugin/Plugin.php
+++ src/Plugin/Plugin.php
@@ -647,6 +647,25 @@ border-collapse: collapse;
         }
 
         if ( $template == '' ) {
+
+            if ( $this->isDevEnv() ) {
+                if ( in_array( pathinfo( $_SERVER['REQUEST_URI'], PATHINFO_EXTENSION ),
+                    [ 'jpg', 'jpeg', 'png', 'gif' ] ) ) {
+
+                    preg_match( '/(.+)-([0-9]+)x([0-9]+)\.(jpg|jpeg|png|gif)$/', $_SERVER['REQUEST_URI'], $matches );
+
+
+                    $w = isset( $matches[2] ) ? $matches[2] : 1024;
+                    $h = isset( $matches[3] ) ? $matches[3] : 768;
+
+                    header( 'Location: https://placebeard.it/' . $w . '/' . $h );
+                }
+
+                if ( pathinfo( $_SERVER['REQUEST_URI'], PATHINFO_EXTENSION ) == 'svg' ) {
+                    header( 'Location: http://placeholder.pics/svg/300/DEDEDE/555555/SVG' );
+                }
+            }
+            
             return;
         }
 
@@ -658,24 +677,6 @@ border-collapse: collapse;
         if ( in_array( pathinfo( $_SERVER['REQUEST_URI'], PATHINFO_EXTENSION ),
             [ 'jpg', 'jpeg', 'png', 'gif' ] ) ) {
             $mv = new Version( $_SERVER['REQUEST_URI'] );
-        }
-
-        if ( $this->isDevEnv() ) {
-            if ( in_array( pathinfo( $_SERVER['REQUEST_URI'], PATHINFO_EXTENSION ),
-                [ 'jpg', 'jpeg', 'png', 'gif' ] ) ) {
-
-                preg_match( '/(.+)-([0-9]+)x([0-9]+)\.(jpg|jpeg|png|gif)$/', $_SERVER['REQUEST_URI'], $matches );
-
-
-                $w = isset( $matches[2] ) ? $matches[2] : 1024;
-                $h = isset( $matches[3] ) ? $matches[3] : 768;
-
-                header( 'Location: https://placebeard.it/' . $w . '/' . $h );
-            }
-
-            if ( pathinfo( $_SERVER['REQUEST_URI'], PATHINFO_EXTENSION ) == 'svg' ) {
-                header( 'Location: http://placeholder.pics/svg/300/DEDEDE/555555/SVG' );
-            }
         }
 
         $view = View::make( 'Layout.' . $view_name );
