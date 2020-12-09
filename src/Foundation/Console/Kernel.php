26d12d20d1dd185c650722f83c95b6d55e6e0dc3
Ben Kremer <ben.kremer@me.com>
2020-12-09 13:35:04 +0100

    Kernel stubb without errors

---
+++ src/Foundation/Console/Kernel.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Sloth\Foundation\Console;
+
+class Kernel implements \Illuminate\Contracts\Console\Kernel
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function handle($input, $output = null)
+    {
+        // TODO: Implement handle() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function call($command, array $parameters = [], $outputBuffer = null)
+    {
+        // TODO: Implement call() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function queue($command, array $parameters = [])
+    {
+        // TODO: Implement queue() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function all()
+    {
+        // TODO: Implement all() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function output()
+    {
+        // TODO: Implement output() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function terminate($input, $status)
+    {
+        // TODO: Implement terminate() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function bootstrap()
+    {
+        // TODO: Implement bootstrap() method.
+    }
+}
