b90b2d88bd16fe396ae7355bda47c8f78ccb9a84
Ben Kremer <git@benkremer.de>
2023-12-18 11:10:43 +0100

    fix: PHP Deprecated: parse_str(): Passing null to parameter #1

--- src/Api/Controller.php
+++ src/Api/Controller.php
@@ -4,36 +4,58 @@ namespace Sloth\Api;
 
 use Sloth\Utility\Utility;
 
-class Controller {
+class Controller
+{
     protected $request;
     public $response;
 
-    public function index() {
+    /**
+     * @return array
+     */
+    public function index()
+    {
         return [];
     }
 
-    final public function __construct() {
-        $this->response          = new \stdClass();
-        $this->response->status  = 200;
+    /**
+     * @param $id
+     *
+     * @return array
+     */
+    final public function __construct()
+    {
+        $this->response = new \stdClass();
+        $this->response->status = 200;
         $this->response->headers = [];
     }
 
-    final public function setRequest( \WP_REST_Request $request ) {
+    /**
+     * @param \WP_REST_Request $request
+     * @return void
+     */
+    final public function setRequest(\WP_REST_Request $request)
+    {
         $this->request = $request;
     }
 
-    final protected function getUrl( string $path, array $params = [] ) {
+    /**
+     * @param $id
+     *
+     * @return array
+     */
+    final protected function getUrl(string $path, array $params = [])
+    {
 
-        parse_str( parse_url( $path, PHP_URL_QUERY ), $get_array );
+        parse_str((string)parse_url($path, PHP_URL_QUERY), $get_array);
 
-        $params = array_merge( $get_array, $params );
+        $params = array_merge($get_array, $params);
 
-        $path = '/sloth/v1/' . Utility::viewize( ( new \ReflectionClass( $this ) )->getShortName() ) . '/' . $path;
-        if ( count( $params ) ) {
-            $path .= '?' . http_build_query( $params );
+        $path = '/sloth/v1/' . Utility::viewize((new \ReflectionClass($this))->getShortName()) . '/' . $path;
+        if (count($params)) {
+            $path .= '?' . http_build_query($params);
         }
 
-        return get_rest_url( null, $path );
+        return get_rest_url(null, $path);
     }
 
 }
