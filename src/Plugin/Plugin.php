e74e0e872be2723a48fd0e7500edcab8c799ab7f
Ben Kremer <git@benkremer.de>
2026-03-19 21:00:21 +0100

    fix: restore loadClassFromFile with Corcel class filtering
    - Use reflection to find classes defined in the file
    - Skip Corcel base classes
    - Prefer App\ namespace classes
    - Support global namespace classes (like BlogInfo)

--- src/Plugin/Plugin.php
+++ src/Plugin/Plugin.php
@@ -163,20 +163,26 @@ class Plugin extends Singleton
      */
     protected function loadClassFromFile(string $file): string
     {
-        $filename = basename($file, '.php');
-        $namespace = 'App\\' . str_replace(DIR_APP, '', dirname($file));
-        $namespace = str_replace(DS, '\\', $namespace);
-        $namespace = rtrim($namespace, '\\');
-
-        $className = $namespace . '\\' . $filename;
-
-        if (class_exists($className)) {
-            return $className;
-        }
-
+        $file = realpath($file);
         include_once $file;
 
-        return $className;
+        $matchingClass = null;
+
+        foreach (get_declared_classes() as $class) {
+            $rc = new \ReflectionClass($class);
+            if ($rc->getFilename() === $file) {
+                if (strpos($class, 'Corcel\\') === 0) {
+                    continue;
+                }
+                if (strpos($class, 'App\\') === 0) {
+                    $matchingClass = $class;
+                    break;
+                }
+                $matchingClass = $class;
+            }
+        }
+
+        return $matchingClass ?? '';
     }
 
     /**
