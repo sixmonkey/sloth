eea871a2d773a851b80cfe6342a735c2e0a44d60
Ben Kremer <git@benkremer.de>
2026-04-15 21:14:09 +0200

    style: compact constructor in AcfProxy

--- src/ACF/AcfProxy.php
+++ src/ACF/AcfProxy.php
@@ -4,9 +4,7 @@ namespace Sloth\ACF;
 
 class AcfProxy
 {
-    public function __construct(private $fields)
-    {
-    }
+    public function __construct(private $fields) {}
 
     public function __call($name, $arguments)
     {
