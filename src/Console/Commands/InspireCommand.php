bd32425378a6e0f97b3dd752067748719961de5c
Ben Kremer <git@benkremer.de>
2026-04-21 19:12:10 +0200

    refactor(console): Simplify and redesign InspireCommand output

--- src/Console/Commands/InspireCommand.php
+++ src/Console/Commands/InspireCommand.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Sloth\Console\Commands;
 
-use function Termwind\render;
 use Sloth\Console\Command;
 
 /**
@@ -20,28 +19,15 @@ class InspireCommand extends Command
 
     public function handle(): int
     {
-        render(<<<'HTML'
-            <div class="mt-2">
-                <div class="flex gap-2">
-                    <span class="text-green-500">
-                         _
-                        | |
-                        | |__  __ _  _ __  _   _  _ __
-                        | '_ \/ _` || '_ \| | | || '_ \
-                        | | | | (_) | | | | | | || | | |
-                        |_| |_|\__,_|_| |_||_| |_||_| |_|
-                    </span>
-                </div>
-                <div class="mt-1 text-center text-gray-500">
-                    Sloth WordPress Framework
-                </div>
-                <div class="mt-2 text-center">
-                    <span class="bg-green-400 text-black px-2">
-                        Hello World! 🚀
-                    </span>
-                </div>
+        \Termwind\render(<<<'HTML'
+            <div class="py-1 ml-2">
+                <div class="px-1 bg-red-300 text-black">🦥  Sloth</div>
+                <em class="ml-1">
+                  May the sloth be with you.
+                </em>
             </div>
-        HTML);
+        HTML
+        );
 
         return self::SUCCESS;
     }
