1773b577cabbd51242c88e8816a5796a3b70e5bf
Ben Kremer <git@benkremer.de>
2026-05-01 15:20:31 +0200

    feat: Introduce SlothDebugBar and refactor DebugServiceProvider

--- src/Debug/DebugServiceProvider.php
+++ src/Debug/DebugServiceProvider.php
@@ -4,163 +4,43 @@ declare(strict_types=1);
 
 namespace Sloth\Debug;
 
-use DebugBar\DataCollector\ExceptionsCollector;
-use DebugBar\DataCollector\MessagesCollector;
-use DebugBar\DataCollector\TimeDataCollector;
 use DebugBar\DebugBar;
-use DebugBar\DebugBarException;
-use Illuminate\Contracts\Container\BindingResolutionException;
-use Illuminate\Contracts\Debug\ExceptionHandler as ExceptionHandlerContract;
-use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Support\Str;
 use Sloth\Core\ServiceProvider;
-use Sloth\Debug\Collectors\AcfCollector;
-use Sloth\Debug\Collectors\QueryCollector;
-use Sloth\Debug\Collectors\SlothCollector;
 
-/**
- * Sloth Debug Service Provider.
- *
- * Registers PHP DebugBar for development environments and custom
- * exception handling for the Sloth WordPress theme.
- *
- * @since 1.0.0
- * @see \Sloth\Debug\ExceptionHandler
- */
 class DebugServiceProvider extends ServiceProvider
 {
-    protected $timeCollector;
-    protected $messagesCollector;
-    protected $exceptionsCollector;
-    public function __construct($app)
-    {
-        $startTime = defined('SLOTH_START') ? (float) SLOTH_START : microtime(true);
-
-        $this->app = $app;
-        $this->timeCollector = new TimeDataCollector($startTime);
-        $this->messagesCollector = new MessagesCollector();
-        $this->exceptionsCollector = new ExceptionsCollector();
-    }
-
-    /**
-     * Register the service provider.
-     *
-     * Registers custom exception handling and initializes PHP DebugBar
-     * when available.
-     *
-     * @since 1.0.0
-     */
     public function register(): void
     {
-        $this->publishes([
-            __DIR__ . '/../../config/debugger.php' => $this->app->configPath('debugger.php'),
-        ], 'config');
+        $this->app->singleton(SlothDebugBar::class);
+        $this->app->alias(SlothDebugBar::class, 'debugbar');
+        $this->app->alias(SlothDebugBar::class, DebugBar::class);
 
-        $this->app->singleton(
-            ExceptionHandlerContract::class,
-            ExceptionHandler::class
-        );
-
-        if (class_exists(DebugBar::class)) {
-            ob_start([$this, 'appendBar']);
-            $this->app->singleton(DebugBar::class, function () {
-                return new DebugBar();
-            });
-        }
+        ob_start([$this, 'renderBar']);
     }
 
-    /**
-     * Boots the DebugServiceProvider
-     *
-     * @throws DebugBarException
-     * @throws FileNotFoundException
-     * @throws BindingResolutionException
-     */
     public function boot(): void
     {
         $this->mergeConfigFrom(
-            __DIR__ . '/../../config/debugger.php',
+            __DIR__ . '/config/debugger.php',
             'debugger'
         );
-        $this->configureDebugBar();
-    }
 
-    /**
-     * Configure the PHP DebugBar.
-     *
-     * Sets up the debug bar with custom collectors for Sloth,
-     * WordPress, ACF, and database queries.
-     *
-     * @return void
-     * @throws DebugBarException
-     * @throws BindingResolutionException|FileNotFoundException
-     * @since 1.0.0
-     */
-    private function configureDebugBar(): void
-    {
-        $debugBar = $this->app->make(DebugBar::class);
-        $debugBar->getJavascriptRenderer()->setBaseUrl('https://php-debugbar.com/assets/');
-
-        collect(config('debugger.bar.collector_providers', []))
-            ->each(function ($provider) use (&$debugBar) {
-                (new $provider($debugBar))->boot();
-            });
-
-        $debugBar->addCollector(new SlothCollector());
-        $debugBar->addCollector(new QueryCollector());
-        $debugBar->addCollector(new AcfCollector());
-
-        $renderer = $debugBar->getJavascriptRenderer();
-
-        $renderer->addInlineAssets(
-            app('files')->get(__DIR__ . '/../../resources/sloth-debugbar.css'),
-            '',
-            ''
-        );
-
-        $this->app->instance('debugbar', $debugBar);
-    }
-
-    /**
-     * Render the PHP DebugBar HTML.
-     *
-     * Renders the debug bar JavaScript and CSS for injection
-     * into the page HTML.
-     *
-     * @return string The rendered debug bar HTML.
-     * @since 1.0.0
-     */
-    private function renderBar(): string
-    {
+        // Resolve the SlothDebugBar instance during boot to force it to be loaded in the Octane sandbox
         try {
-            $debugBar = $this->app->make('debugbar');
-            $renderer = $debugBar->getJavascriptRenderer();
-            return $renderer->renderHead() . "\n" . $renderer->render();
-        } catch (\Throwable) {
-            return '';
+            $debugBar = $this->app->make(SlothDebugBar::class);
+            $debugBar->boot();
+        } catch (\Throwable $e) {
+            return;
         }
     }
 
-    /**
-     * Append the debug bar to page output.
-     *
-     * Injects the debug bar HTML into the page by replacing
-     * the closing </head> tag or appending to the body.
-     *
-     * @param string $output The page HTML output.
-     * @return string The output with debug bar injected.
-     * @since 1.0.0
-     */
-    private function appendBar($output): string
+    public function renderBar($output): string
     {
-        if (Str::contains($output, '</head>')) {
-            return Str::replace('</head>', $this->renderBar() . '</head>', $output);
-        }
-        return $output . $this->renderBar();
-    }
-
-    public function __destruct()
-    {
-        // TODO: Implement __destruct() method.
+        return Str::replace(
+            '</head>',
+            $this->app->make(SlothDebugBar::class)->render() . '</head>',
+            $output
+        );
     }
 }
---
+++ src/Debug/SlothDebugBar.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sloth\Debug;
+
+use DebugBar\DataCollector\ExceptionsCollector;
+use DebugBar\DataCollector\MessagesCollector;
+use DebugBar\DataCollector\TimeDataCollector;
+use DebugBar\DebugBar;
+use Sloth\Core\Application;
+
+class SlothDebugBar extends DebugBar
+{
+    protected TimeDataCollector $timeCollector;
+    protected MessagesCollector $messagesCollector;
+    protected ExceptionsCollector $exceptionsCollector;
+
+    protected bool $booted = false;
+
+    public function __construct(protected Application $app)
+    {
+        $this->timeCollector = new TimeDataCollector();
+        $this->messagesCollector = new MessagesCollector();
+        $this->exceptionsCollector = new ExceptionsCollector();
+    }
+
+    public function boot(): void
+    {
+        if ($this->booted) {
+            return;
+        }
+
+        collect(config('debugger.bar.collector_providers', []))
+            ->each(function ($collectorProvider) {
+                (new $collectorProvider($this))->boot();
+            });
+
+        $this->booted = true;
+    }
+
+    public function getTimeCollector(): TimeDataCollector
+    {
+        return $this->timeCollector;
+    }
+
+    public function getMessagesCollector(): MessagesCollector
+    {
+        return $this->messagesCollector;
+    }
+
+    public function getExceptionsCollector(): ExceptionsCollector
+    {
+        return $this->exceptionsCollector;
+    }
+
+    /**
+     * Magic calls for adding messages
+     */
+    public function __call(string $method, array $args): void
+    {
+        $messageLevels = ['emergency', 'alert', 'critical', 'error', 'warning', 'notice', 'info', 'debug', 'log'];
+        if (in_array($method, $messageLevels, true)) {
+            foreach ($args as $arg) {
+                $this->addMessage($arg, $method);
+            }
+        }
+    }
+
+    /**
+     * Adds a message to the MessagesCollector
+     *
+     * A message can be anything from an object to a string
+     */
+    public function addMessage(mixed $message, string $label = 'info', array $context = []): void
+    {
+        $this->messagesCollector->addMessage($message, $label, $context);
+    }
+
+    public function render(): string
+    {
+        $renderer = $this->getJavascriptRenderer();
+        $renderer->setBaseUrl('https://php-debugbar.com/assets/');
+        $renderer->addInlineAssets(
+            app('files')->get(__DIR__ . '/resources/sloth-debugbar.css'),
+            '',
+            ''
+        );
+        return $renderer->renderHead() . "\n" . $renderer->render();
+    }
+}
