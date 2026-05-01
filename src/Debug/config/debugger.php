1d0589222f4eb2b998e1a920127cd590daf47222
Ben Kremer <git@benkremer.de>
2026-05-01 15:20:31 +0200

    refactor: Relocate Debug Bar configuration and assets to src/Debug

--- src/Debug/config/debugger.php
+++ src/Debug/config/debugger.php
@@ -2,8 +2,13 @@
 
 return [
     'bar' => [
+        'display' => app()->isLocal(),
         'collector_providers' => [
             \Sloth\Debug\CollectorProviders\MessageCollectorProvider::class
         ]
+    ],
+    'json' => [
+        'prepend' => true,
+        'key' => '__debug'
     ]
 ];
