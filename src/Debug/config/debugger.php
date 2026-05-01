24352fae40c96651302e120521bd1fadd4cf3f5d
Ben Kremer <git@benkremer.de>
2026-05-01 18:20:43 +0200

    refactor: Update Debugbar configuration and enable new collectors

--- src/Debug/config/debugger.php
+++ src/Debug/config/debugger.php
@@ -1,12 +1,21 @@
 <?php
 
 return [
-    'editor' => 'vscode',
+    'editor' => 'phpstorm',
     'bar' => [
         'display' => app()->isLocal(),
         'collector_providers' => [
-            \Sloth\Debug\CollectorProviders\MessageCollectorProvider::class
-        ]
+            \Sloth\Debug\CollectorProviders\MessageCollectorProvider::class,
+            \Sloth\Debug\CollectorProviders\PdoCollectorProvider::class,
+            \Sloth\Debug\CollectorProviders\SlothCollectorProvider::class,
+            \Sloth\Debug\CollectorProviders\AcfCollectorProvider::class,
+            \Sloth\Debug\CollectorProviders\WordpressCollectorProvider::class,
+            \Sloth\Debug\CollectorProviders\QueryCollectorProvider::class,
+
+            \Sloth\Debug\CollectorProviders\PhpInfoCollectorProvider::class,
+            \Sloth\Debug\CollectorProviders\MemoryCollectorProvider::class,
+        ],
+        'dump_all' => true
     ],
     'json' => [
         'prepend' => true,
