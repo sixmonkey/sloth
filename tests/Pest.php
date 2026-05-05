dc4805eb42e704e0bd46dc20e325e5a96cfcbee3
Ben Kremer <git@benkremer.de>
2026-05-05 17:13:20 +0200

    test(console): Introduce helper for console testing

--- tests/Pest.php
+++ tests/Pest.php
@@ -7,6 +7,7 @@
  */
 
 use Brain\Monkey;
+use Sloth\Core\Application;
 
 beforeEach(function (): void {
     Monkey\setUp();
@@ -15,3 +16,23 @@ beforeEach(function (): void {
 afterEach(function (): void {
     Monkey\tearDown();
 });
+
+/**
+ * Create a minimal test Application for console tests.
+ *
+ * @return Application The configured test application.
+ * @since 1.0.0
+ */
+function makeTestApp(): Application
+{
+    $app = new Application();
+    $app->instance('config', new \Illuminate\Config\Repository([]));
+    $app->instance('files', new \Illuminate\Filesystem\Filesystem());
+    $app->instance('events', new \Illuminate\Events\Dispatcher($app));
+    $app->instance('path.base', sys_get_temp_dir());
+    $app->instance('path.app', sys_get_temp_dir() . '/app');
+    $app->instance('path.theme', sys_get_temp_dir() . '/theme');
+    $app->instance('path.cache', sys_get_temp_dir() . '/cache');
+
+    return $app;
+}
